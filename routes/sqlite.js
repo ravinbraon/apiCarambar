3948b3d4a144cd96e4446277c571c7bd932f9b9e
ravinbraon <127830878+ravinbraon@users.noreply.github.com>
2024-10-24 09:29:07 +0200

    wip

--- routes/sqlite.js
+++ routes/sqlite.js
@@ -29,4 +29,26 @@ sequelize.sync()
   })
   .catch(err => console.error('Erreur lors de la synchronisation de la base de données :', err));
 
+
+/*sequelize.sync({ force: true })
+  .then(() => {
+    console.log('Base de données et tables créées !');
+    return Jokes.bulkCreate([
+        { joke: 'Quelle est la femelle du hamster ? L’Amsterdam' },
+        { joke: 'Que dit un oignon quand il se cogne ? Aïe' },
+        { joke: "Quel est l'animal le plus heureux ? Le hibou, parce que sa femme est chouette." },
+        { joke: "Pourquoi le football c'est rigolo ? Parce que Thierry en rit"},
+        { joke: "Quel est le sport le plus fruité ? La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes."},
+        { joke: "Que se fait un Schtroumpf quand il tombe ? Un Bleu "},
+        { joke: "Quel est le comble pour un marin ? Avoir le nez qui coule"},
+        { joke: "Qu'est ce que les enfants usent le plus à l'école ? Le professeur"},
+        { joke: "Quel est le sport le plus silencieux ? Le para-chuuuut"},
+        { joke: "Quel est le comble pour un joueur de bowling ? C’est de perdre la boule"}
+      ]);
+  })
+  .then(jokeEntry => {
+    console.log("Blague ajoutée :", jokeEntry.joke);
+  })
+  .catch(err => console.error('Erreur lors de la synchronisation de la base de données :', err));
+*/
 module.exports = router
